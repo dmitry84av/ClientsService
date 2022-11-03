20d63bdea7549a048b86370dfcb62e97332bde84
dmitry84av <dp041184vda@gmail.com>
2022-11-03 19:48:25 +0200

    Init START

--- src/main/java/com/example/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/example/clientsservice/ClientsServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientsServiceApplication {
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ClientsServiceApplication.class, args);
+		System.out.println("START");
 	}
 }
