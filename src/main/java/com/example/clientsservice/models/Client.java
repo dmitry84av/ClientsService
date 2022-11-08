464c714b49897016086219f9a99e933cf3863dc3
dmitry84av <dp041184vda@gmail.com>
2022-11-08 20:55:02 +0200

    relation Client Phone

--- src/main/java/com/example/clientsservice/models/Client.java
+++ src/main/java/com/example/clientsservice/models/Client.java
@@ -1,16 +1,14 @@
 package com.example.clientsservice.models;
 
-import jdk.jfr.Enabled;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
 //
 @Entity
 @Table(name = "clients")
@@ -27,9 +25,12 @@ public class Client {
     private String name;
     @Column(length = 50, nullable = false)
     private String patronymic;
+    @Column(nullable = false, columnDefinition = "int(1) default 0")
     private Gender gender;
     @Column(length = 50, nullable = false, unique = true)
     private String email;
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+    private Set<Phone> phones;
 
 
 }
