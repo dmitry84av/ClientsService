e0e43584d73d9eef9f5da091d84e978f83227983
dmitry84av <dp041184vda@gmail.com>
2022-11-08 21:42:56 +0200

    relation Client Account

--- src/main/java/com/example/clientsservice/models/Client.java
+++ src/main/java/com/example/clientsservice/models/Client.java
@@ -3,6 +3,8 @@ package com.example.clientsservice.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -25,12 +27,34 @@ public class Client {
     private String name;
     @Column(length = 50, nullable = false)
     private String patronymic;
-    @Column(nullable = false, columnDefinition = "int(1) default 0")
+    @Column(nullable = false, columnDefinition = "int default 0")
     private Gender gender;
     @Column(length = 50, nullable = false, unique = true)
     private String email;
     @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
     private Set<Phone> phones;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "clients_accounts",
+            joinColumns = @JoinColumn(name = "client_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "FK_clients")),
+            inverseJoinColumns = @JoinColumn(name = "account_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "FK_accounts"))
+    )
+    private Set<Account> accounts;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(surname, client.surname) && Objects.equals(name, client.name) && Objects.equals(patronymic, client.patronymic) && gender == client.gender && Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, surname, name, patronymic, gender, email);
+    }
 
     @Override
     public String toString() {
