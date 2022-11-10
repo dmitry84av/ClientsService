3a7df3cb76e1f56be7a7e640ec81cd724d28f900
dmitry84av <dp041184vda@gmail.com>
2022-11-10 14:00:30 +0200

    relation Client Account

--- src/main/java/com/example/clientsservice/models/Client.java
+++ src/main/java/com/example/clientsservice/models/Client.java
@@ -1,16 +1,16 @@
 package com.example.clientsservice.models;
 
-import jdk.jfr.Enabled;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
 //
 @Entity
 @Table(name = "clients")
@@ -27,9 +27,44 @@ public class Client {
     private String name;
     @Column(length = 50, nullable = false)
     private String patronymic;
+    @Column(nullable = false, columnDefinition = "int default 0")
     private Gender gender;
     @Column(length = 50, nullable = false, unique = true)
     private String email;
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+    private Set<Phone> phones;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "clients_accounts",
+            joinColumns = @JoinColumn(name = "client_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "FK_clients")),
+            inverseJoinColumns = @JoinColumn(name = "account_id", nullable = false,
+                    foreignKey = @ForeignKey(name = "FK_accounts"))
+    )
+    private Set<Account> accounts;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) && Objects.equals(surname, client.surname) && Objects.equals(name, client.name) && Objects.equals(patronymic, client.patronymic) && gender == client.gender && Objects.equals(email, client.email);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, surname, name, patronymic, gender, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
