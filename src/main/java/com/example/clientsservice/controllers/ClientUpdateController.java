efe28282f3e842255e9d43b6736540d7edc3b68b
dmitry84av <dp041184vda@gmail.com>
2022-11-22 19:39:26 +0200

    mustache clientsUpdateController

---
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -0,0 +1,21 @@
+package com.example.clientsservice.controllers;
+
+import com.example.clientsservice.models.Client;
+import com.example.clientsservice.srvices.data.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ClientUpdateController {
+    @Autowired
+    private ClientService clientService;
+    @GetMapping ("clientUpdate")
+    public String load(@RequestParam("clientId")Integer id, Model model) {
+        Client client = clientService.findById(id);
+        model.addAttribute("client",client);
+        return "clientUpdate";
+    }
+}
--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -5,7 +5,11 @@ import com.example.clientsservice.srvices.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -24,5 +28,10 @@ public class ClientsController {
         List<Client> list = clientService.findAll();
         model.addAttribute("clients",list);
         return "clients";
+    }
+    @PostMapping("thisClientForm")
+    public ModelAndView thisClientForm(@RequestParam("id") Integer id) {
+        return new ModelAndView("clientUpdate", new ModelMap("clientId",id));
+
     }
 }
