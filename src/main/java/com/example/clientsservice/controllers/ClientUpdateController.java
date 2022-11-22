3480e0cfcdae746fcb6efe3edbb2ff5ff0104f6c
dmitry84av <dp041184vda@gmail.com>
2022-11-22 21:04:31 +0200

    mustache clientsUpdate genders auto

--- src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class ClientUpdateController {
     @Autowired
@@ -16,6 +19,10 @@ public class ClientUpdateController {
     public String load(@RequestParam("clientId")Integer id, Model model) {
         Client client = clientService.findById(id);
         model.addAttribute("client",client);
+       for (Gender value : Client.Gender.values()) {
+           genders.put(value.name(),value == client.getGender() ? "selected" : "");
+       }
+       model.addAttribute("genders",genders.entrySet());
         return "clientUpdate";
     }
 }
