c62c5e6954107450d7aa691bb8c2f5e5c28ec839
dmitry84av <dp041184vda@gmail.com>
2022-11-29 21:57:04 +0200

    thymeleaf main contoler client controler clients HTML add client update client form

---
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -0,0 +1,35 @@
+package com.example.clientsservice.controllers;
+
+import com.example.clientsservice.models.Client;
+import com.example.clientsservice.srvices.data.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ClientUpdateController {
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("clientUpdate")
+    public String load(@RequestParam("id") Integer id, Model model) {
+        Client client = clientService.findById(id);
+        model.addAttribute("client",client);
+        model.addAttribute("genders",Client.Gender.values());
+        return "clientUpdate";
+    }
+    @PostMapping("updateClientForm")
+    public ModelAndView method(@ModelAttribute Client client){
+        System.err.println(client);
+        clientService.save(client);
+        return new ModelAndView("redirect:clientUpdate",
+                new ModelMap("id",client.getId()));
+
+    }
+}
--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -5,9 +5,12 @@ import com.example.clientsservice.srvices.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -28,5 +31,9 @@ public class ClientsController {
 	public String addClientForm(@ModelAttribute Client client) {
 	clientService.save(client);
 		return "redirect:clients";
+	}
+	@PostMapping("openClientForm")
+	public ModelAndView openClientForm(@RequestParam("id")Integer id){
+		return new ModelAndView("redirect:clientUpdate", new ModelMap("id",id));
 	}
 }
