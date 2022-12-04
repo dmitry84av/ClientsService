a72d15138da7d9dcee46bdfc42e5e4595c7ea7a6
dmitry84av <dp041184vda@gmail.com>
2022-12-04 21:24:08 +0200

    mustache clientAddForm done

--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,5 +26,19 @@ public class ClientsController {
         List<Client> list = clientService.findAll();
         model.addAttribute("clients",list);
         return "clients";
+    }
+    @PostMapping("clientAddForm")
+    public String clientAddForm(
+            @RequestParam("surname") String surname,
+            @RequestParam("name") String name,
+            @RequestParam("patronymic") String patronymic,
+            @RequestParam("gender") Client.Gender gender,
+            @RequestParam("email") String email
+    ) {
+        Client client = new Client(0, surname, name, patronymic, gender, email,
+                null, null, null);
+        clientService.save(client);
+        return "redirect:";
+
     }
 }
