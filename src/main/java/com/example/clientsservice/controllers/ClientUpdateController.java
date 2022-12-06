86ea1ea76bcd23673a54c2d7d7056f9f5db119e8
dmitry84av <dp041184vda@gmail.com>
2022-12-06 16:05:33 +0200

    mustache clientUpdate gender

--- src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -1,12 +1,16 @@
 package com.example.clientsservice.controllers;
 
 import com.example.clientsservice.models.Client;
+import com.example.clientsservice.models.Client.Gender;
 import com.example.clientsservice.srvices.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ClientUpdateController {
@@ -16,6 +20,9 @@ public class ClientUpdateController {
     public String load(@RequestParam("clientId") Integer id, Model model) {
         Client client = clientService.findById(id);
         model.addAttribute("client", client);
+        for (Gender value: Gender.values()){
+              model.addAttribute(value.name(), value==client.getGender()? "selected" : "");
+        }
         return "clientUpdate";
     }
 }
