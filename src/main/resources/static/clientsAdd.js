39a48b0b41aabb4390ecaf04f0dcf47e21be3720
dmitry84av <dp041184vda@gmail.com>
2022-12-08 19:43:50 +0200

    thymeleaf clients async ajax clientObject

---
+++ src/main/resources/static/clientsAdd.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded',function (){
+    document.getElementById('submitAddClientForm').onclick = function (ev){
+        ev.preventDefault()
+        ajaxSubmitForm()
+    }
+})
+function ajaxSubmitForm(){
+    let form =document.getElementById('addClientForm')
+    let formData = new FormData(form)
+    let json=JSON.stringify(Object.fromEntries(formData))
+    //
+    let request=new XMLHttpRequest()
+        request.open('post','/rest/addClientForm')
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
+    request.send(json)
+    request.addEventListener('readystatechange',function (){
+        if (request.readyState !==4)
+            return
+        if (request.status===200){
+            let clients = JSON.parse(request.responseText)
+            clients = {'clients': clients}
+        }
+    })
+
+}
