d6e3ea6eff42de51d6efac4ac2906cd14d7453a1
dmitry84av <dp041184vda@gmail.com>
2022-12-14 19:30:37 +0200

    user get by id polyjilos obnovlayet bazy

--- src/main/java/com/example/clientsservice/controllers/UserUpdateController.java
+++ src/main/java/com/example/clientsservice/controllers/UserUpdateController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 import java.util.HashMap;
@@ -33,7 +37,12 @@ public class UserUpdateController {
             status.put(value, value == user.getStatus() ? "selected" : "");
         }
         model.addAttribute("status", status.entrySet());
-
         return "userUpdate";
+    }
+    @PostMapping("userUpdateForm")
+    public  ModelAndView userUpdateForm(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return new ModelAndView("redirect:userUpdate",
+                new ModelMap("userId", user.getId()));
     }
 }
