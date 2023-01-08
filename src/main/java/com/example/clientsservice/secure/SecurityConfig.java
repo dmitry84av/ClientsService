187bb5a1f30ca4ef97b05eff714c48c0ebaeb35c
dmitry84av <dp041184vda@gmail.com>
2023-01-08 20:39:12 +0200

    thymeleaf_navbar_users register 02

---
+++ src/main/java/com/example/clientsservice/secure/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.clientsservice.secure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.clientsservice.models.User.Role.ADMIN;
+import static com.example.clientsservice.models.User.Role.USER;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemory(BCryptPasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .passwordEncoder(encoder::encode)
+                        .username("u")
+                        .password("u")
+                        .roles(USER.name())
+                        .build(),
+                User.builder()
+                        .passwordEncoder(encoder::encode)
+                        .username("a")
+                        .password("a")
+                        .roles(ADMIN.name())
+                        .build()
+
+        );
+    }
+}
---
+++ src/main/java/com/example/clientsservice/secure/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.clientsservice.secure;
+
+import com.example.clientsservice.models.User;
+import com.example.clientsservice.repositiries.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        System.err.println(user);
+        if (user == null)
+            throw new UsernameNotFoundException(username);
+        return new UserDetailImpl(user);
+    }
+}
