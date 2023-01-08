22844e14e0c851ad17be43ac9d7f04cdbb86efb5
dmitry84av <dp041184vda@gmail.com>
2023-01-08 21:24:40 +0200

    thymeleaf_navbar_users register 04

---
+++ src/main/java/com/example/clientsservice/secure/AuthProvider.java
@@ -0,0 +1,53 @@
+package com.example.clientsservice.secure;
+
+import com.example.clientsservice.models.User;
+import com.example.clientsservice.repositiries.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Component
+public class AuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        System.err.println("AUTHENTICATE");
+        User user = userRepository.findByUsername(authentication.getName());
+        System.err.println(user);
+        if (user == null)
+            throw new BadCredentialsException("wrong username");
+            if (!passwordEncoder.matches(
+                    (String) authentication.getCredentials(),
+                    user.getPassword())
+            )
+                throw new BadCredentialsException("wrong password");
+        return new UsernamePasswordAuthenticationToken(
+                user,
+                authentication.getCredentials(),
+
+                Arrays.stream(User.Role.values())
+                        .map(role -> new SimpleGrantedAuthority(role.name()))
+                        .collect(Collectors.toSet())
+        );
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
